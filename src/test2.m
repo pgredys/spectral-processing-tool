1ec59060496952340e0c406213861d736e33deab
Felixater <gredys.przemyslaw@gmail.com>
2022-05-18 23:50:38 +0200

    Data export added

--- src/test2.m
+++ src/test2.m
@@ -6,7 +6,7 @@ fs    = 10;
 t     = 0 : 1/fs : 40; 
 x     = sin(2*pi*3*t) + rand(1, length(t))*10; 
  figure;   
-[Power, Frequency] = WelchPSD(x,[], Hann(length(t))* 0.25, .5, fs); 
+[Power, Frequency] = WelchPSD(x,[], Hann(length(t))* 0.25, 1, fs); 
 plot(gca, Frequency, 10*log10(abs(Power)))
 title(gca, 'Welch Cross Power Spectral Density Estimate') 
 xlabel(gca, 'Frequency (Hz)')
